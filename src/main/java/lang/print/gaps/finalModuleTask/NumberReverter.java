8015b660207450eaaa2ce8ed1b7c6c6b8eb99ea4
Bekhzod <Bekhzod1411@gmail.com>
2022-10-04 20:51:51 +0500

    new
    new

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+ int first = number%10*100;
+ int second = number%100/10*10;
+ int third = number /100;
+        System.out.println(first+second+third);
+
     }
 }
